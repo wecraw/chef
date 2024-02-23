a5dd131699678c8510e70d017644f157985e4af8
wecraw <william.crawford6@t-mobile.com>
2024-02-22 17:15:45 -0800

    more menu stuff, functioning now

--- src/app/home2/home2.component.ts
+++ src/app/home2/home2.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { chefNames } from '../chefNames';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-home2',
@@ -8,4 +9,10 @@ import { chefNames } from '../chefNames';
 })
 export class Home2Component {
   chefNames = chefNames;
+
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  public onClick(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
 }
