e036b8f2906b0675520a3c3799103bf0cb06d552
wecraw <william.crawford6@t-mobile.com>
2024-02-24 14:42:38 -0800

    menu style

--- src/app/home2/home2.component.ts
+++ src/app/home2/home2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { chefNames } from '../chefNames';
 import { ViewportScroller } from '@angular/common';
 
@@ -9,8 +9,30 @@ import { ViewportScroller } from '@angular/common';
 })
 export class Home2Component {
   chefNames = chefNames;
-
+  scrollZone = 0;
   constructor(private viewportScroller: ViewportScroller) {}
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    // Calculate the scroll position
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    // Define thresholds for different scroll zones
+    const zoneThresholds = [0, 600, 1200, 1500]; // Change these values as per your requirement
+
+    // Find the current scroll zone based on scroll position
+    for (let i = 0; i < zoneThresholds.length; i++) {
+      if (scrollPosition < zoneThresholds[i]) {
+        console.log('Scroll Zone:', i);
+        this.scrollZone = i - 1;
+        break;
+      }
+    }
+  }
 
   public onClick(elementId: string): void {
     this.viewportScroller.scrollToAnchor(elementId);
